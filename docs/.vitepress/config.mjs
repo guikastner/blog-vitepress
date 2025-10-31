d967859558c535c5156b43667d4b3f548311d78f
guikastner <guilherme@kastner.com.br>
2025-10-31 15:35:40 -0300

    update

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,10 +2,11 @@ export default {
   title: "Dicas do Kastner",
   description: "Blog pessoal com VitePress",
   base: "/", // domínio raiz: kastner.com.br
+  cleanUrls: 'with-subfolders',
   themeConfig: {
     nav: [
       { text: "Início", link: "/" },
-      { text: "Guia", link: "/guide" },
+      { text: "Guia", link: "/guide/" },
     ],
   },
 };
